8eb333215d5f546ff98d0029bd96bf2e9a044ab6
EricRocaC <rocacepero98@gmail.com>
2019-10-01 19:55:32 +0200

    Practica2_Llista

---
+++ Practica2_EricRoca/llista.py
@@ -0,0 +1,3 @@
+import random
+llista = [random.randrange(0,100), random.randrange(0,100), random.randrange(0,100), random.randrange(0,100), random.randrange(0,100), random.randrange(0,100), random.randrange(0,100), random.randrange(0,100), random.randrange(0,100), random.randrange(0,100)]
+print(llista)
