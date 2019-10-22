49cb4b79207e4544a0f7476861633b030c6c322b
EricRocaC <rocacepero98@gmail.com>
2019-10-22 19:14:21 +0200

    Equacio1.py

--- Practica2_EricRoca/Equacio1.py
+++ Practica2_EricRoca/Equacio1.py
@@ -5,26 +5,16 @@ class EquacioPrimerGrau:
     def calcula(self):
         p = self.seq.split()
         a = p[0]
-<<<<<<< HEAD
         a = a[:-1]
-=======
-        a = a[1-1]
->>>>>>> ed5e2b310b4b5ee287f45a9f0e6e684ec4c19d17
         b = p[2]
         operador = p[1]
         c = p[4]
         r = ""
         if operador == "+":
-<<<<<<< HEAD
             r = (float(c)-float(b))/float(a)
         elif operador == "-":
             r = (float(c)+float(b))/float(a)
-            
-=======
-            r = (int(c)-int(b))/int(a)
-        elif operador == "-":
-            r = (int(c)+int(b))/int(a)
->>>>>>> ed5e2b310b4b5ee287f45a9f0e6e684ec4c19d17
+
         print(self.seq)
         print("x = "+str(r))
 
