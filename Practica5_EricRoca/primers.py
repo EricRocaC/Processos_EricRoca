dbb899fdc00c9133d85174c09f0361d81c1a1e02
EricRocaC <rocacepero98@gmail.com>
2019-10-22 19:19:46 +0200

    DocumentacióPrimers

--- Practica5_EricRoca/primers.py
+++ Practica5_EricRoca/primers.py
@@ -6,7 +6,7 @@ class llista_primers:
     """
     Creem una llista per a afegir-hi els nombres primers començant per el dos
     i sumant-li continuament fins a un màxim de números demanats per l'usuari.
-    >>> llista_primers(5).l
+    >>> llista_primers(5).llista
     """
     def __init__(self, n):
         """inicialitzem la variable n i creem la llista buida i el buscador."""
@@ -15,14 +15,6 @@ class llista_primers:
         self.busca()
 
     def busca(self):
-        """
-        Dins d'aquest apartat ens dedicarem a treballar amb els ifs.
-        El primer if afegirà un 2 dins de la llista buida.
-        El segón comprobarà si la llista és més petita que el número introduit per l'usuari i donarà pas al bucle.
-        El while comprobarà si "trobat" és "false" (cas que es donará, com deiem abans, quan la llista sigui igual al número introduit).
-        El for farà un "mod" comprobant que el residu entre el número que estem utilitzant i l'últim a la llista sigui 0.
-        Si ho es, l'afegirà a la llista. Un com acabada la llista, el programa sortirà del bucle i acabarà.
-        """
         if (len(self.llista) == 0):
             #si la llista continua buida, aquest if li afegira un 2 com a primer parametre de la llista.
             self.llista.append(2)
